d40607288a7625412483188df2c5d686d2e16d8e
fob1493 <fob1493@gmail.com>
2018-05-21 11:50:36 +0100

    Privacy Policy

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import IndexLayout from './component/pages/IndexLayout';
 import SearchDisplay from './component/pages/SearchDisplay';
 import AboutUs from './component/pages/About';
 import NoMatch from './component/pages/NoMatch';
+import PrivacyPolicy from './component/pages/PrivacyPolicy';
 
 const App = () => (
   <div className="">
     <Switch>
       <Route exact path="/" component={IndexLayout} />
-      <Route exact path="/aboutus" component={AboutUs} />
-      <Route exact path="/search/" component={SearchDisplay} />
+      <Route path="/aboutus" component={AboutUs} />
+      <Route path="/search/" component={SearchDisplay} />
+      <Route path="/privacy" component={PrivacyPolicy} />
       <Route component={NoMatch} />
     </Switch>
   </div>
